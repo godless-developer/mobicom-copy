a86946f541737905fe6f35a067a3e73b8c08ef9f
godless-developer <bbtulga98@gmail.com>
2025-02-10 14:14:27 +0800

    hjkl;

--- src/app/_components/HomePage.tsx
+++ src/app/_components/HomePage.tsx
@@ -1,15 +1,17 @@
 import { EasyGrid } from "./EasyGrid";
 import { Header } from "./Header";
+import { NeedHelp } from "./NeedHelp";
 import { OnlineShop } from "./OnlineShop";
 import { Special } from "./Special";
 
 export const HomePage = () => {
   return (
-    <div className=" rounded-lg  h-full p-2 flex flex-col gap-6 bg-background ">
+    <div className=" rounded-lg  h-full p-2 flex flex-col gap-6  ">
       <Header />
       <EasyGrid />
       <Special />
       <OnlineShop />
+      <NeedHelp />
     </div>
   );
 };
